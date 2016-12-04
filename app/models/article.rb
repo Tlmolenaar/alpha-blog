073f53efa7c9e2b368eabae835f739bb08b06f86
beautiful_loser <tlmolenaar@hotmail.com>
2016-12-04 21:49:35 +0000

    linked user db to articles db

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :title, presence: true, length:{minimum: 1, maximum: 10}
   validates:description, presence: true, length:{minimum: 10, maximum: 2000}
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-
+  has_many :articles
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, presence: true, length: {minimum: 1, maximum: 50 },
+            uniqueness: {case_sensitive: false},
+            format: {with: VALID_EMAIL_REGEX}
+            
+  
 end
