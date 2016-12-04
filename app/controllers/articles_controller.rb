d01199399bdb3b56d1e4064b08b53721c2da8b5b
beautiful_loser <tlmolenaar@hotmail.com>
2016-12-04 16:02:09 +0000

    updates controllers and added links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,25 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles =  Article.all
+  end
+  
   def new
     @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Succesfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
   
   def create
@@ -13,9 +32,18 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
+  
+  
   
   private
     def article_params
